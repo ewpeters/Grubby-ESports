da0aa1fd80924d7bef48060e1d2488d6dc0e7047
Erik Petersen <erik.w.petersen86@gmail.com>
2011-06-18 16:19:35 -0700

    changed Upcoming Events styling; changed mkdir logic for articles to absolue paths; removed @themes references

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -62,8 +62,8 @@ class Admin::ArticlesController < Admin::ApplicationController
     folder_name = "#{@article.html.original_filename.gsub(/\..*/, '')}"
     
     
-    unless File.exists?("public/html_files/#{folder_name}")
-      Dir.mkdir("public/html_files/#{folder_name}")
+    unless File.exists?("#{Rails.root}/public/html_files/#{folder_name}")
+      Dir.mkdir("#{Rails.root}/public/html_files/#{folder_name}")
     end
     
     FileUtils.mv(params[:file].path, "public/html_files/#{folder_name}/#{params[:qqfile]}")  
