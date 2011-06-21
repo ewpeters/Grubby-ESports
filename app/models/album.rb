5130930cf1b5c1dbfcb86ae650170abb47f9862d
Erik Petersen <erik.w.petersen86@gmail.com>
2011-06-20 21:09:16 -0700

    added a default scope to albums; changed layout of album pic editiing

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,6 +2,7 @@ class Album < ActiveRecord::Base
   validates_presence_of :title, :description
   acts_as_list
   acts_as_taggable_on :tags
+  default_scope :order => "created_at DESC"
   has_many :photos, :order => "position"
   has_one :cover, :class_name => "Photo", :foreign_key => "cover_id"
 end
